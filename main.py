0dc8bdc8ede589dfadd340f19797ccc03f312200
charlotte <71281295+chaarlottte@users.noreply.github.com>
2023-01-26 17:22:40 -0500

    fuck you clumsy

--- main.py
+++ main.py
@@ -1,123 +1,29 @@
-from customtkinter import filedialog
-from idlelib.tooltip import Hovertip
-from threading import Thread
-import requests as reqs
-import customtkinter
-import subprocess
-import json
-import os
+import subprocess, os
 
-app = customtkinter.CTk()
-app.geometry("500x250")
-app.title("By Clumsy#2304")
-app.resizable(False, False)
+paths = f"./Monsoon/Monsoon.json"
+mcDir = f"{os.path.expanduser( '~' )}/AppData/Roaming/.minecraft"
+assetsDir = f"{mcDir}/assets"
+monsoonJarfile = "Monsoon/mc/Monsoon.jar"
+jvmPath = f"{os.getenv('JAVA_HOME')}\\bin\\java.exe"
+ram = "8000" # go fuck yourself if you have less you bitch
+arguments = []
 
-filename = ""
-ram = 6
+# JVM Start command
+arguments.append(jvmPath)
 
-pathss = f"{os.path.expanduser( '~' )}\\AppData\\Roaming\\.minecraft\\Rise6Launcher\\cum.json"
+# VM args
+arguments.append(f"-noverify -Xms512m -Xmx{ram}m -Djava.library.path=\"Monsoon/natives/\" -cp \"Monsoon/libraries/*;{monsoonJarfile}\"") 
 
+# Main class
+arguments.append(f"net.minecraft.client.main.WrappedLaunchLauncher")
 
-os.makedirs(f"{os.path.expanduser( '~' )}\\AppData\\Roaming\\.minecraft\\Rise6Launcher", exist_ok=True)
-if os.path.isfile(pathss):
-    with open(pathss, "r") as cum:
-        cum = json.load(cum)
-        filename = cum["file_path"]
-        ram = cum["ram"]
-else:
-    with open(pathss, "w") as cum:
-        json.dump({"ram": "6", "file_path": ""}, cum)
+# MC Args
+arguments.append(f"--width 854 --height 400 --username haiku_is_a_cutie --version 1.8.9 --gameDir {mcDir} --assetsDir {assetsDir} --assetIndex 1.8.9 --uuid N/A --accessToken trolololol --userType mojang")
 
+command = ""
+for arg in arguments:
+    command = command + arg + " "
+command = command + ""
 
-def browseFiles():
-    global filename
-    filename = filedialog.askdirectory()
-    with open(pathss, "w") as cum:
-        json.dump({"ram": ram, "file_path": filename}, cum)
-
-
-def launchRise():
-    global filename
-    ram = str(int(ram_slider.get()))
-    with open(pathss, "w") as cum:
-        json.dump({"ram": ram, "file_path": filename}, cum)
-    if not filename:
-        createTopLevel(app, "Select a file path")
-    elif not os.path.isfile(f"{filename}\\files\\azul-1.8.9_345\\bin\\java.exe"):
-        createTopLevel(app, "JDK was not found in the supplied file path")
-    else:
-        command = fr'{filename}\files\azul-1.8.9_345\bin\java.exe -noverify -Xms512m -Xmx{ram}g -Djava.library.path={filename}\files\1.8.9-natives-win -cp "{filename}\files\RiseCompressed.jar;lwjgl.jar;lwjgl_util.jar" net.minecraft.client.main.Main -uuid fc5bc365-aedf-30a8-8b89-04e462e29bde -accessToken yes -version 1'
-        subprocess.Popen(command, shell=True, cwd=f"{os.path.expanduser( '~' )}\\AppData\\Roaming\\.minecraft", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-
-def downloadAudio():
-<<<<<<< HEAD
-    if os.path.isdir(f"{os.path.expanduser( '~' )}\\AppData\\Roaming\\.minecraft\\resourcepacks"):
-        fix_btn.configure(state="disabled", text="Downloading")
-    if os.path.isdir("LauncherFiles\\resourcepacks"):
-=======
-    try:
->>>>>>> parent of 8515176 (fuck i want to be raped and drenched in cum so badly rn)
-        fix_btn.config(state="disabled", text="Downloading")
-        req = reqs.get(url='https://lopsidedheavyvirus.steamtest083.repl.co/risetexture.zip')
-        with open(f"{os.path.expanduser( '~' )}\\AppData\\Roaming\\.minecraft\\resourcepacks\\risetexture.zip", 'wb') as f:
-            f.write(req.content)
-        createTopLevel(app, "Succesfully Installed Pack")
-    except:
-        createTopLevel(app, "Resource Pack Folder Not Found Please Launch Rise")
-    
-    fix_btn.config(state="enabled", text="Audio Fix")
-
-
-def audioFix():
-    dl_thread = Thread(target=downloadAudio)
-    dl_thread.start()
-
-
-def createTopLevel(app, msg):
-    window = customtkinter.CTkToplevel(app)
-    window.title(msg)
-    window.geometry("425x150")
-    window.resizable(False, False)
-
-    label = customtkinter.CTkLabel(master=window, text=msg)
-    label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
-
-
-bigFont = ("product_sans", 20)
-smallFont = ("product_sans", 16)
-
-# Launch Shit
-launch_btn = customtkinter.CTkButton(master=app, text="Launch", font=bigFont, command=launchRise)
-launch_btn.place(relx=0.2, rely=0.8, anchor=customtkinter.CENTER)
-myTip = Hovertip(launch_btn,'This Launches Rise 6')
-
-path_btn = customtkinter.CTkButton(master=app, text="File Path", font=bigFont, command=browseFiles)
-path_btn.place(relx=0.8, rely=0.8, anchor=customtkinter.CENTER)
-myTip = Hovertip(path_btn,'This Lets You Select The Rise 6 Path')
-
-fix_btn = customtkinter.CTkButton(master=app, text="Audio Fix", font=bigFont, command=audioFix)
-fix_btn.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
-myTip = Hovertip(fix_btn,'This Fixes Rise 6 Audio Issue')
-
-title_lbl = customtkinter.CTkLabel(master=app, text="Rise 6 launcher", font=bigFont)
-title_lbl.place(relx=0.5, rely=0.10, anchor=customtkinter.CENTER)
-
-# Ram Shit
-ram_lbl = customtkinter.CTkLabel(master=app, text="Ram Allocation", font=smallFont)
-ram_lbl.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER)
-
-ram_lbl_min = customtkinter.CTkLabel(master=app, text="2gb", font=smallFont)
-ram_lbl_min.place(relx=0.27, rely=0.5, anchor=customtkinter.CENTER)
-
-ram_lbl_mid = customtkinter.CTkLabel(master=app, text="6gb", font=smallFont)
-ram_lbl_mid.place(relx=0.50, rely=0.6, anchor=customtkinter.CENTER)
-
-ram_lbl_max = customtkinter.CTkLabel(master=app, text="10gb", font=smallFont)
-ram_lbl_max.place(relx=0.74, rely=0.5, anchor=customtkinter.CENTER)
-
-ram_slider = customtkinter.CTkSlider(master=app, from_=2, to=10, number_of_steps=4)
-ram_slider.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-ram_slider.set(int(ram))
-
-app.mainloop()
+print(arguments)
+subprocess.call(command)
